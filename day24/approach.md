e946ca7b853bf08dceb106f07fb2e1cb3dbeb1e4
Justin Hou <justinhsg@gmail.com>
2024-12-24 20:34:47 +0800

    day 24

--- day24/approach.md
+++ day24/approach.md
@@ -2,4 +2,50 @@
 
 ## Part 1
 
+At first glance I thought part 2 was going to be trying to figure out what the bit operations actually do, so I decided to just focus on part 1. Just implementing as intended, using a top-down DP to speed up the answer.
+
 ## Part 2
+
+Good news was that this wasn't a 'reverse engineer' the circuit problem, but it's close. I immediately recalled the circuit for a full adder, and it took a while to recall the boolean equations for the outputs:
+
+$$
+\begin{align}
+x_i \space \oplus \space y_i &= t \nonumber\\
+t \space \oplus \space c_{i-1} &= z_i \nonumber\\
+t \space \cdot \space c_{i-1} &= u \nonumber\\
+x_i \space \cdot  \space y_i &= v \nonumber\\
+u \space +  \space v &= c_i \nonumber\\
+\end{align}
+$$
+
+My first instinct was to look at the $z_i$ outputs. If they aren't outputs of XOR operations, then those definitely need to be swapped. I then looked at the remaining XOR equations and identified outputs that should have been swapped.
+
+This gave me 6 wires to swap, and hence there are 2 more.
+
+I updated my code to swap those wires when parsing the input, and then printed out the equations grouped 5 ways:
+
+1) XOR equations with `x` and `y` inputs
+2) XOR equations with `z` outputs
+3) AND equations with `x` and `y` inputs
+4) AND equations without `x` and `y` inputs
+5) OR equations
+
+It was a messy, and required quite a lot of restarts and `fmt.Println` statements to tease out the last pair, and finally got it by iterating upwards and tracking the `carry` wires.
+
+After getting the answer, I did a full rewrite of the code to better get out the answer. I could verify if each of the 5 equations 'types' had a valid output:
+
+$$
+\begin{align}
+x_i \space \oplus \space y_i &= t \nonumber\\
+t \space \oplus \space c_{i-1} &= z_i \nonumber\\
+t \space \cdot \space c_{i-1} &= u \nonumber\\
+x_i \space \cdot  \space y_i &= v \nonumber\\
+u \space +  \space v &= c_i \nonumber\\
+\end{align}
+$$
+
+1. $t$ must be an input to another XOR equation and another AND equation
+2. $z_i$ must start with `z`
+3. $u$ must be an input to an OR equation
+4. $v$ must be an input to an OR equation
+5. $c_i$ must be an input to a XOR equation and another AND equation
--- day24/solution.go
+++ day24/solution.go
@@ -2,15 +2,30 @@ package day24
 
 import (
 	"fmt"
+	"slices"
 	"strconv"
+	"strings"
 
 	"github.com/aoc-2024-go/io"
+	"github.com/aoc-2024-go/types"
+	"github.com/aoc-2024-go/utils"
 )
 
-var part1Answer, part2Answer int
-var lines []string
+var part1Answer int
+var part2Answer string
+var sections [][]string
 
+type Operation = types.Triple[string, string, string]
+type StrPair = types.Pair[string, string]
 type Solution struct{}
+
+var values map[string]int8 = make(map[string]int8)
+var operations map[string]Operation = make(map[string]Operation)
+var nBits int
+var maxZ int
+var andOps map[string]StrPair = make(map[string]StrPair)
+var orOps map[string]StrPair = make(map[string]StrPair)
+var xorOps map[string]StrPair = make(map[string]StrPair)
 
 func (s Solution) Solve(isSample bool, dirName string) (string, string) {
 	var pathToInput string
@@ -20,7 +35,108 @@ func (s Solution) Solve(isSample bool, dirName string) (string, string) {
 	} else {
 		pathToInput = fmt.Sprintf("./%s/input.txt", dirName)
 	}
-	lines = io.ReadFile(pathToInput)
+	sections = io.SplitIntoSections(pathToInput)
 
-	return strconv.Itoa(part1Answer), strconv.Itoa(part2Answer)
+	inputs := sections[0]
+	nBits = len(inputs) / 2
+	for _, line := range inputs {
+		var inputName string
+		var valInt int
+		parts := strings.Split(line, ": ")
+		inputName = parts[0]
+		valInt, _ = strconv.Atoi(parts[1])
+		values[inputName] = int8(valInt)
+	}
+
+	for _, line := range sections[1] {
+
+		var op1, op, op2, result string
+		fmt.Sscanf(line, "%s %s %s -> %s", &op1, &op, &op2, &result)
+		if result[0] == 'z' {
+			zId, _ := strconv.Atoi(result[1:])
+			maxZ = utils.Max(zId, maxZ)
+		}
+
+		switch op {
+		case "AND":
+			andOps[op1] = StrPair{Fst: op2, Snd: result}
+			andOps[op2] = StrPair{Fst: op1, Snd: result}
+		case "OR":
+			orOps[op1] = StrPair{Fst: op2, Snd: result}
+			orOps[op2] = StrPair{Fst: op1, Snd: result}
+
+		case "XOR":
+			xorOps[op1] = StrPair{Fst: op2, Snd: result}
+			xorOps[op2] = StrPair{Fst: op1, Snd: result}
+		}
+		operations[result] = Operation{Fst: op1, Snd: op, Thd: op2}
+	}
+
+	for i := range maxZ + 1 {
+		wire := fmt.Sprintf("z%02d", i)
+		val := evaluateVar(wire)
+		part1Answer = part1Answer | (int(val) << i)
+	}
+
+	if isSample {
+		return strconv.Itoa(part1Answer), part2Answer
+	}
+
+	var anomalies map[string]bool = make(map[string]bool)
+
+	for from, pair := range xorOps {
+		if from[0] == 'x' || from[0] == 'y' {
+			internal := pair.Snd
+			if internal == "z00" {
+				continue
+			}
+			if xorOps[internal] == (StrPair{}) || andOps[internal] == (StrPair{}) {
+				anomalies[internal] = true
+			}
+		} else {
+			output := pair.Snd
+			if output[0] != 'z' {
+				anomalies[output] = true
+			}
+		}
+	}
+
+	for _, pair := range andOps {
+		output := pair.Snd
+		if output[0] == 'z' {
+			anomalies[output] = true
+		}
+	}
+
+	for _, pair := range orOps {
+		output := pair.Snd
+		if output[0] == 'z' && output != fmt.Sprintf("z%02d", nBits) {
+			anomalies[output] = true
+		}
+	}
+
+	var sortedAnomalies []string
+	for key := range anomalies {
+		sortedAnomalies = append(sortedAnomalies, key)
+	}
+	slices.Sort(sortedAnomalies)
+	part2Answer = strings.Join(sortedAnomalies, ",")
+	return strconv.Itoa(part1Answer), part2Answer
+}
+
+func evaluateVar(wire string) int8 {
+	_, evaluated := values[wire]
+	if !evaluated {
+		var operation Operation = operations[wire]
+		op1, op, op2 := operation.Fst, operation.Snd, operation.Thd
+		switch op {
+		case "AND":
+			values[wire] = evaluateVar(op1) & evaluateVar(op2)
+		case "OR":
+			values[wire] = evaluateVar(op1) | evaluateVar(op2)
+		case "XOR":
+			values[wire] = evaluateVar(op1) ^ evaluateVar(op2)
+		}
+	}
+	return values[wire]
 }
